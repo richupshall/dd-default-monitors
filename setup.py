abc93382bdf388e06185491600f426b0aafb2dad
Rich Upshall <rich.upshall@datadoghq.com>
2018-07-30 11:30:56 +0100

    remove keys from correct file

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 import os
 import requests
 
-API_KEY='56487bcfe9bdd8db3431c89b914e287a'
-APPLICATION_KEY='2cd1beada177f84e1c53902b6dfc07f2e7009c62'
+API_KEY=''
+APPLICATION_KEY=''
 
 url='https://app.datadoghq.com/api/v1/monitor?api_key=%s&application_key=%s' % (API_KEY,APPLICATION_KEY)
 
